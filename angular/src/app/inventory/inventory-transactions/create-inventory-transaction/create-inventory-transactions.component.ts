c4722b6e21fc6241e100a1d4d310664ce7b4ca16
John Doe <jazal@cipherlabz.com>
2021-09-04 21:30:27 +0530

    rendering the line level into a angular material table

--- angular/src/app/inventory/inventory-transactions/create-inventory-transaction/create-inventory-transactions.component.ts
+++ angular/src/app/inventory/inventory-transactions/create-inventory-transaction/create-inventory-transactions.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit, Injector } from "@angular/core";
 import { finalize } from "rxjs/operators";
 import { AppComponentBase } from "@shared/app-component-base";
 import {
-  ProductionServiceProxy,
   InventoryServiceProxy,
   CreateGoodsReceivedDto,
+  CreateGoodsReceivedProductDto,
+  CommonKeyValuePairDto,
 } from "@shared/service-proxies/service-proxies";
 import { forEach as _forEach, map as _map } from "lodash-es";
 import { appModuleAnimation } from "@shared/animations/routerTransition";
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
   selector: "app-create-inventory-transactions",
@@ -21,10 +23,25 @@ export class CreateInventoryTransactionsComponent
 {
   saving = false;
   grn = new CreateGoodsReceivedDto();
+  displayedColumns: string[] = [
+    "product-name",
+    "quantity",
+    "free-quantity",
+    "unit-price",
+    "selling-price",
+    "mrp-price",
+    "discount-rate",
+    "discount-amount",
+    "line-amount",
+  ];
+
+  LINE_LEVEL_DATA: CreateGoodsReceivedProductDto[] = [];
+  dataSource = new MatTableDataSource<CreateGoodsReceivedProductDto>(
+    this.LINE_LEVEL_DATA
+  );
 
   constructor(
     injector: Injector,
-    private _productionService: ProductionServiceProxy,
     private _inventoryService: InventoryServiceProxy
   ) {
     super(injector);
@@ -32,12 +49,35 @@ export class CreateInventoryTransactionsComponent
 
   ngOnInit(): void {}
 
-  selectedSupplier($event) {
+  selectedSupplier($event: CommonKeyValuePairDto) {
     console.log($event);
   }
 
-  selectedProducts($event) {
+  selectedProducts($event: CommonKeyValuePairDto) {
     console.log($event);
+
+    let l = new CreateGoodsReceivedProductDto();
+    l.goodsRecievedId = null;
+    l.goodsRecievedNumber = null;
+    l.sequenceNumber = this.dataSource.data.length + 1;
+    l.productId = $event.id;
+    l.productCode = $event.code;
+    l.productName = $event.name;
+    l.expiryDate = null;
+    l.batchNumber = null;
+    l.quantity = 0;
+    l.freeQuantity = 0;
+    l.costPrice = 0;
+    l.sellingPrice = 0;
+    l.maximumRetailPrice = 0;
+    l.discountRate = 0;
+    l.discountAmount = 0;
+    l.lineTotal = 0;
+    this.dataSource.data.push(l);
+
+    console.log(this.dataSource);
+
+    return (this.dataSource.filter = "");
   }
 
   save(): void {
